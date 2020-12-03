601d43743ceaf7b3ddc7657d43106a9f89cc1e3c
Charlie Appleton <xjarlie@gmail.com>
2020-12-04 08:24:54 +0900

    Implemented cordova

--- firebaseconfig.js
+++
@@ -1,9 +0,0 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyDoxnxXrZrkmvwxqvGOd0HKxwMJgLHum_I",
-    authDomain: "lwttv-8fcd8.firebaseapp.com",
-    databaseURL: "https://lwttv-8fcd8.firebaseio.com",
-    projectId: "lwttv-8fcd8",
-    storageBucket: "lwttv-8fcd8.appspot.com",
-    messagingSenderId: "326986084837",
-    appId: "1:326986084837:web:56b983ce9bab9e397f7f0c"
-};
